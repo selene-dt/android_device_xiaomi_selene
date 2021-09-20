383558b23c394e6c84da840d5e307524eeb3cd2e
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-21 01:58:57 +0300

    selene: sensors: Avoid target name conflicts
    Change-Id: I170c8c7e4edbfcdebf5d6f6df148b72993d805f6
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- device.mk
+++ device.mk
@@ -321,7 +321,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.multihal \
+    android.hardware.sensors@2.1-service.selene-multihal \
     android.frameworks.sensorservice@1.0.vendor \
     libsensorndkbridge
 
