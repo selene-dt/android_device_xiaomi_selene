0da52246a3aa1a32144c64e31dc867f865c3bf2e
bengris32 <bengris32@protonmail.ch>
2023-09-29 13:03:58 +0100

    selene: libinit: Skip variant detection in recovery
    Change-Id: Iebd0d09849b41dc40aeff6529b79a86dc24e412f
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- libinit/init_xiaomi_selene.cpp
+++ libinit/init_xiaomi_selene.cpp
@@ -6,6 +6,7 @@
 
 #include <libinit_variant.h>
 #include <libinit_utils.h>
+#include <unistd.h>
 
 #include "vendor_init.h"
 
@@ -65,11 +66,15 @@ static const variant_info_t selenes_info = {
 };
 
 static const std::vector<variant_info_t> variants = {
-    ruby_global_info,
-    rubypro_global_info,
-    rubyplus_info,
+    eos_global_info,
+    eos_u_global_info,
+    selene_global_info,
+    selene_in_info,
+    selenes_info
 };
 
 void vendor_load_properties() {
-    search_variant(variants);
+    if (access("/system/bin/recovery", F_OK) != 0) {
+        search_variant(variants);
+    }
 }
