eb6e164dd8b0900f7d3b4ff93126772c470e2c23
bengris32 <bengris32@protonmail.ch>
2023-09-30 23:12:13 +0100

    selene: libinit: Set `ro.product.mod_device` property
    Change-Id: I85baa2a62d79526737b5d65aa93f9042262ae6a0
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- libinit/init_xiaomi_selene.cpp
+++ libinit/init_xiaomi_selene.cpp
@@ -16,6 +16,7 @@ static const variant_info_t eos_global_info = {
 
     .brand = "Redmi",
     .device = "eos",
+    .mod_device = "eos_global",
     .marketname = "Redmi 10",
     .model = "21061119DG",
     .build_fingerprint = "Redmi/eos_global/eos:13/TP1A.220624.014/V14.0.4.0.TKUMIXM:user/release-keys"
@@ -27,6 +28,7 @@ static const variant_info_t eos_u_global_info = {
 
     .brand = "Redmi",
     .device = "eos",
+    .mod_device = "eos_u_global",
     .marketname = "Redmi 10 2022",
     .model = "22011119UY",
     .build_fingerprint = "Redmi/eos_global/eos:13/TP1A.220624.014/V14.0.4.0.TKUMIXM:user/release-keys"
@@ -38,6 +40,7 @@ static const variant_info_t selene_global_info = {
 
     .brand = "Redmi",
     .device = "selene",
+    .mod_device = "selene_global",
     .marketname = "Redmi 10",
     .model = "21061119AG",
     .build_fingerprint = "Redmi/selene_global/selene:13/TP1A.220624.014/V14.0.4.0.TKUMIXM:user/release-keys"
@@ -49,6 +52,7 @@ static const variant_info_t selene_in_info = {
 
     .brand = "Redmi",
     .device = "selene",
+    .mod_device = "selene_in_global",
     .marketname = "Redmi 10 Prime",
     .model = "21061119BI",
     .build_fingerprint = "Redmi/selene_in/selene:13/TP1A.220624.014/V14.0.7.0.TKUINXM:user/release-keys"
@@ -60,6 +64,7 @@ static const variant_info_t selenes_info = {
 
     .brand = "Redmi",
     .device = "selenes",
+    .mod_device = "selenes",
     .marketname = "Redmi Note 11 4G",
     .model = "21121119SC",
     .build_fingerprint = "Redmi/selene/selene:13/TP1A.220624.014/V14.0.2.0.TKUCNXM:user/release-keys"
