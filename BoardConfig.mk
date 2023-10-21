f1374cc26f6411e41565616372804c3753994eb4
rio004 <riyon336@gmail.com>
2023-10-21 01:15:10 +0100

    selene: Import compatibility matrix and manifest
    Signed-off-by: rio004 <riyon336@gmail.com>
    Signed-off-by: atharv29511 <atharvnegi1106@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,6 +43,10 @@ TARGET_NO_BOOTLOADER := true
 # Display
 TARGET_SCREEN_DENSITY := 440
 
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
+
 # Kernel
 BOARD_DTB_OFFSET := 0x0bc08000
 BOARD_KERNEL_BASE := 0x40078000
