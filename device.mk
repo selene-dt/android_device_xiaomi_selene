db5e628046ce00f91d0a4d47cc18b9c12e3ddee3
bengris32 <bengris32@protonmail.ch>
2024-02-10 16:00:42 +0530

    selene: Initial reverse engineered Goodix fingerprint 2.1 interfaces
    Change-Id: Idd04d3313a6dbacd649767ff686426de152d576d
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- device.mk
+++ device.mk
@@ -126,6 +126,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.selene
 
+PRODUCT_PACKAGES += \
+    libvendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
+
 # FM
 PRODUCT_PACKAGES += \
     FMRadio
