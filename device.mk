de7a69dc16b8cbb9a4fe0ef57f916d39ab166029
bengris32 <bengris32@protonmail.ch>
2024-02-21 16:12:27 +0530

    selene: Switch to common QTI vibrator service
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- device.mk
+++ device.mk
@@ -393,8 +393,13 @@ PRODUCT_PACKAGES += \
     android.hardware.usb.gadget@1.1.vendor
 
 # Vibrator
+$(call soong_config_set, vibrator, vibratortargets, vibratoraidlV2target)
+
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator-service.mediatek
+    vendor.qti.hardware.vibrator.service
+
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
