26a9f66dea707d6bca219e3da1ce7b52b6bfd1d2
Sarthak Roy <sarthakroy2002@gmail.com>
2024-02-21 20:46:44 +0530

    selene: props: Purge lsc_nvram logspam
    * Silence:
    
    Change-Id: Id68e8b0876e432dc1a89d10d8ca87f444fd7479e
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- configs/props/vendor_logtag.mk
+++ configs/props/vendor_logtag.mk
@@ -261,3 +261,4 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.log.tag.netd=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.gralloc4=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.MDP=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.lsc_nvram=$(VENDOR_LOG_LEVEL)
