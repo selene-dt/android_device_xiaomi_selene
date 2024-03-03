a8a0e8331a60ce864341230ef1ccf4960e32f3a2
bengris32 <bengris32@protonmail.ch>
2024-03-03 05:45:16 +0530

    selene: Switch back to stock Perf configs
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- device.mk
+++ device.mk
@@ -313,9 +313,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.mediatek.hardware.mtkpower@1.2.vendor
 
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/perf,$(TARGET_COPY_OUT_VENDOR)/etc)
-
 # Power Off Alarm
 PRODUCT_PACKAGES += \
     PowerOffAlarm
