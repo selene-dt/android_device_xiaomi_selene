58d4e0dcdcf027e74e8eb13e1ab5b515d4e9cf80
Jaimin Kalariya <jaimin.kalariya@siliconsignals.io>
2024-03-06 09:31:11 +0100

    selene: props: Silence OpenGLRenderer logspam
    E OpenGLRenderer: Unable to match the desired swap behavior
    
    Change-Id: I13c5b79e0cfb3b5e3430b65c6dc0d821093127f2
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- configs/props/vendor_logtag.mk
+++ configs/props/vendor_logtag.mk
@@ -261,4 +261,5 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.log.tag.netd=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.gralloc4=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.MDP=$(VENDOR_LOG_LEVEL) \
-    persist.log.tag.lsc_nvram=$(VENDOR_LOG_LEVEL)
+    persist.log.tag.lsc_nvram=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.OpenGLRenderer=$(VENDOR_LOG_LEVEL)
